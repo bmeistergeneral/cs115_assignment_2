f2486d2c1ce1b280ec7bf6b1fa22731d7d71a340
bmeistergeneral <brecon.morgan@gmail.com>
2023-07-18 15:21:44 -0400

    Completed subtask 1

--- LinearSearchDemo.java
+++ LinearSearchDemo.java
@@ -12,16 +12,27 @@ public class LinearSearchDemo {
 
     // Main program to test the linearSearch() method
     public static void main(String[] args) {
-        int[] numbers = {2, 4, 7, 10, 11, 32, 45, 87};
-        System.out.print("NUMBERS: ");
+
+        int[] numbers = new int[10];
+        System.out.println("Enter 10 integer values.");
+        Scanner scnr = new Scanner(System.in);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("Enter number %d: ", (i + 1));
+            numbers[i] = scnr.nextInt();
+        }
+
+        System.out.println("Array elements are: ");
+
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
         }
-        System.out.println();
-        Scanner scnr = new Scanner(System.in);
-        System.out.print("Enter an integer value: ");
+
+        System.out.println("\n" + "Input a number to search: ");
+
         int key = scnr.nextInt();
         int keyIndex = linearSearch(numbers, key);
+
         if (keyIndex == -1) {
             System.out.println(key + " was not found.");
         } else {
